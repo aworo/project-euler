dbf707b3599b3b683abf306d505598e2bad911a4
aworo <ajworoniuk@gmail.com>
2015-11-01 23:42:48 -0800

    Problem 5 started

---
+++ p5.js
@@ -0,0 +1,17 @@
+/*!
+Project Euler
+http://projecteuler.net/
+
+
+Problem 5: Smallest Multiple
+"2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?"
+
+Solution by: A.Woroniuk 
+
+Completed? Np
+
+Notes&Thoughts: I'm fairly sure I could have used better JS functions to clean up my palindromeCheck function. Ah well.
+
+
