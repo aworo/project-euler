39f27e3df2ef64faa8bc2606863878b9656f9032
aworo <ajworoniuk@gmail.com>
2015-11-01 23:09:02 -0800

    Problem 3 Finished, with errors (number to large for browser?)

--- p3.js
+++ p3.js
@@ -11,21 +11,34 @@ What is the largest prime factor of the number 600851475143 ?
 
 Solution by: A.Woroniuk 
 
-Completed? No
+Completed? Yes* (*Browser/JS not the best way to handle a number this large!) 
+I'm leaving it listed as unsolved on ProjectEuler.net for now, I tested it with various numbers and it does work correctly as expected.
+
+
 
  *
  */
 
-var number = 600851475143
-var largestprimefactor
+var number = 600851475
+var largestprimefactor = 0
 
- for (var i = 0; i < number; i++) {
- 	if (number % i == 0) {
+ for (var i = 2; i < (number / 2); i++) {
+ 	if (number % i == 0) {  /* if a factor then...*/
+ 		var primefactor = true;
  		for (var j = 2; j < i; j++)	 {
- 			if (i % j != 0) {
- 				largestprimefactor = i;
+ 			if (i % j == 0) {
+ 				primefactor = false;
+ 				break;
  			}
+ 		}	
+ 		if (primefactor == true) {
+ 			largestprimefactor = i;
+ 		document.write(largestprimefactor + "\n");
+
  		}
+ 			
+ 	 		
  	}
 
  }
+document.write(largestprimefactor)
