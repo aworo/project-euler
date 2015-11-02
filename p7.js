1cd0c04cf7b0f7e621f35ad7def18ad0db7a5f49
aworo <ajworoniuk@gmail.com>
2015-11-02 00:19:46 -0800

    Problem 7 started

---
+++ p7.js
@@ -0,0 +1,17 @@
+/*!
+Project Euler
+http://projecteuler.net/
+
+
+Problem 7: 10001s prime
+"By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?"
+
+Solution by: A.Woroniuk 
+
+Completed? No
+
+Notes&Thoughts: 
+
+*/
